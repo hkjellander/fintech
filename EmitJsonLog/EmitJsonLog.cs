7a9c6c1f53ca78488f308df9fe5100f10fa0075c
Henrik Kjellander <henrik.kjellander@gmail.com>
2017-11-07 14:18:43 +0100

    Change to fanout based producer+consumer.
    Set the queues to be non-exclusive queue to survive consumer restart
    
    Make it possible to run multiple ReceiveLogs instances
    in order to consume the same events in different queues.

---
+++ EmitJsonLog/EmitJsonLog.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+using System.Threading;
+
+class EmitJsonLog
+{
+    public static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory()
+        {
+            HostName = "localhost",
+        };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.ExchangeDeclare(exchange: "logs", type: "fanout");
+
+            // Send a bunch of messages.
+            int NBR_MESSAGES = 100;
+            Console.WriteLine("Will send {0} messages", NBR_MESSAGES);
+            for (int i = 0; i < NBR_MESSAGES; i++)
+            {
+                var message = GetMessage(i);
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "logs", 
+                                     routingKey: "my-key", 
+                                     basicProperties: null, body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+                Thread.Sleep(1000);
+            }
+        }
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+
+    private static string GetMessage(int index)
+    {
+        return "{ \"data\": \"" + index + "\" }";
+    }
+}
---
+++ ReceiveLogs/ReceiveLogs.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+class ReceiveLogs
+{
+    public static int Main(string[] args)
+    {
+        if (args.Length != 1)
+        {
+            Console.Error.WriteLine("Usage: {0} <id>", Environment.GetCommandLineArgs()[0]);
+            Console.Error.WriteLine("Where the id argument is mandatory.");
+            return 1;
+        }
+        var id = args[0];
+        var factory = new ConnectionFactory() { 
+            HostName = "localhost",
+        };
+
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.ExchangeDeclare(exchange: "logs",
+                                    type: "fanout");
+            bool durable = true;
+            bool exclusive = false;
+            bool autoDelete = false;
+            var queue = channel.QueueDeclare("my-queue-" + id,
+                                             durable, exclusive,
+                                             autoDelete, null);
+                                        
+            var queueName = queue.QueueName;
+            channel.QueueBind(queue: queueName,
+                              exchange: "logs",
+                              routingKey: "my-key");
+
+            Console.WriteLine(" [*] ID: {0} Waiting for messages.", id);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(" [x] {0}", message);
+            };
+            channel.BasicConsume(queue: queueName, autoAck: true,
+                                 consumer: consumer);
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+            return 0;
+        }
+    }
+}
